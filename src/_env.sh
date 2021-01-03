349266ecb031f2792d08e548297a843e5a0f38f5
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2021-01-03 18:26:00 +0100

    fixed env

--- src/_env.sh
+++ src/_env.sh
@@ -1,3 +1,3 @@
 export CROSS_COMPILE=arm-mobiaqua-linux-gnueabi-
 export BUILD_CC=cc
-export CFLAGS=-march=armv7-a -mthumb -mfpu=neon -mfloat-abi=hard
+export CFLAGS="-march=armv7-a -mthumb -mfpu=neon -mfloat-abi=hard"
